cce56e7fde56e4ef2049093e9bb5751bea6a59fe
Alexander Jaferey <alexj97@gmail.com>
2018-08-04 22:31:07 -0400

    i messed up

---
+++ ping-median.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env Python3.6
+
+import subprocess
+import time
+import sys
+from statistics import median
+
+nums = []
+
+url = input("Address to ping> ")
+
+ping = subprocess.run(["ping", "-c 6", url],
+        encoding='utf-8',
+        stdout=subprocess.PIPE
+        )
+
+ping = ping.stdout.splitlines()
+
+for line in ping:
+	if line.endswith("ms") and line.startswith("64"):
+		line = line[-9:-3]
+		num = float(line)
+		nums.append(num) 
+middle = median(nums)
+print(f"Median time is {middle} ms")
