5b07b43a0d4a44e5415595525afb2d0186d9cbee
Alexander Jaferey <alexj97@gmail.com>
2018-08-08 17:32:10 -0400

    trying new method for capturing output

--- mping.py
+++ mping.py
@@ -14,10 +14,13 @@ parser.add_argument("address", help="Address to ping", nargs="?")
 args = parser.parse_args()
 
 if args.address:
-    ping = subprocess.run(["ping", "-c 6", args.address],
+    ping = subprocess.check_output(["ping", "-c 6", args.address],
             encoding='utf-8',
-            stdout=subprocess.PIPE
-            )
+           )
+    #ping = subprocess.run(["ping", "-c 6", args.address],
+    #        encoding='utf-8',
+    #        stdout=subprocess.PIPE
+    #        )
 else:
     url = input("Address to ping: ")
     ping = subprocess.run(["ping", "-c 5", url],
