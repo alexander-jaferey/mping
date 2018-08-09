9e94f59c20df6cbf3fa0c4f728cf44ba72a2287a
Alexander Jaferey <alexj97@gmail.com>
2018-08-08 21:39:42 -0400

    added short option

--- mping.py
+++ mping.py
@@ -9,18 +9,15 @@ from statistics import median
 nums = []
 
 parser = argparse.ArgumentParser(description="Get median ping time to an address")
-parser.add_argument("address", help="Address to ping", nargs="?") 
-
+parser.add_argument("--short", "-s", action="store_true", help="shortened output")
+parser.add_argument("address", help="Address to ping", nargs="?")
 args = parser.parse_args()
 
 if args.address:
-    ping = subprocess.check_output(["ping", "-c 6", args.address],
+    ping = subprocess.run(["ping", "-c 6", args.address],
             encoding='utf-8',
-           )
-    #ping = subprocess.run(["ping", "-c 6", args.address],
-    #        encoding='utf-8',
-    #        stdout=subprocess.PIPE
-    #        )
+            stdout=subprocess.PIPE
+            )
 else:
     url = input("Address to ping: ")
     ping = subprocess.run(["ping", "-c 5", url],
@@ -31,19 +28,22 @@ else:
 ping = ping.stdout.splitlines()
 
 for line in ping:
-	if line.endswith("ms") and line.startswith("64"):
-		line = line[-9:-3]
-		num = float(line)
-		nums.append(num) 
+    if line.endswith("ms") and line.startswith("64"):
+        line = line[-10:-3]
+        num = float(line.strip("= "))
+        nums.append(num) 
 
 if nums != []:
     middle = "/" + str(median(nums))
 
-for line in ping:
-    if not line.startswith("round"):
-        print(line)
-    else:
-        words = line.split()
-        words[1] += "/med"
-        words[3] += middle
-        stdout.write(" ".join(words))
+if args.short:
+    print(f"Median ping time is {middle.strip('/')}")
+else:
+    for line in ping:
+        if not line.startswith("round"):
+            print(line)
+        else:
+            words = line.split()
+            words[1] += "/med"
+            words[3] += middle
+            stdout.write(" ".join(words))
